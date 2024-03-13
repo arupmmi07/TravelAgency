35a0f403472bf2870f9580a18f7d128bad68e499
Arup Kumar Sarkar <arupmmi07@gmail.com>
2024-03-14 01:39:59 +0530

    TravelAgency Base Structure Added

---
+++ app.py
@@ -0,0 +1,42 @@
+from crewai import Crew, Process
+from textwrap import dedent
+from agents.agents import TravelAgents
+from tasks import TravelTasks
+
+from TravelAgency import TravelAgency
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# This is the main function that you will use to run your custom crew.
+if __name__ == "__main__":
+    print("## Welcome to Trip Planner Crew")
+    print('-------------------------------')
+    origin = input(
+        dedent("""
+      From where will you be traveling from?
+    """))
+    cities = input(
+        dedent("""
+      What are the cities options you are interested in visiting?
+    """))
+    date_range = input(
+        dedent("""
+      What is the date range you are interested in traveling?
+    """))
+    noofdays = input(
+        dedent("""
+      For how many day you are interested in traveling?
+    """))
+    interests = input(
+        dedent("""
+      What are some of your high level interests and hobbies?
+    """))
+
+    travel_agency = TravelAgency(origin, cities, date_range, interests)
+    result = travel_agency.run()
+    print("\n\n########################")
+    print("## Here is you Trip Plan")
+    print("########################\n")
+    print(result)
