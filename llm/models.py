de015926af205be818f33776dc41695824f35ad1
arupmmi07 <arupmmi07@gmail.com>
2024-03-15 20:31:55 +0530

    New models added

--- llm/models.py
+++ llm/models.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-
+import os
 
 def llm_models(modelname='gpt-4'):
     if modelname == 'gpt-4':
@@ -7,8 +7,8 @@ def llm_models(modelname='gpt-4'):
     elif modelname == 'gpt-3.5':
         return ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
     elif modelname == 'llama2' or modelname == 'autoagent-llama2':
-        return ChatOpenAI(model_name="autoagent-llama2", base_url="http://localhost:11434/v1" , temperature=0.7)  
+        return ChatOpenAI(model_name="autoagent-llama2", base_url=os.environ['OPENAI_API_BASE'] , temperature=0.7)  
     elif modelname == 'mistral' or modelname == 'autoagent-mistral':
-        return ChatOpenAI(model_name="autoagent-llama2", base_url="http://localhost:11434/v1" , temperature=0.7)
+        return ChatOpenAI(model_name="autoagent-llama2", base_url=os.environ['OPENAI_API_BASE'] , temperature=0.7)
     else:
         return ChatOpenAI(model_name="gpt-4", temperature=0.7)
