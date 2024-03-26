5aa3d19b754ef0c0d0803a06410b9562d62df39f
arupmmi07 <arupmmi07@gmail.com>
2024-03-27 00:44:16 +0530

    test updaed with pydentic model for better results

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 """Test Agent creation and execution basic functionality."""
 
 from unittest.mock import MagicMock, patch
+import uuid
 
-from pydantic import BaseModel, Field
+from pydantic import UUID4, BaseModel, Field
 from pydantic_core import ValidationError
 
 from crewai import Agent, Crew, Process, Task
@@ -28,12 +29,20 @@ class Output(BaseModel):
     agent: str = Field(
         description="Name of the agent"
     )
+    role: str = Field(
+        description="Role of the agent"
+    )
     tasks: list[str] = Field(
         description="list of tasks performed by the agent"
     )
+    id: UUID4 = Field(
+        default_factory=uuid.uuid4,
+        frozen=True,
+        description="Unique identifier for the object, not set by user.",
+    )
 
 
-agency_type = "Travel Planner"
+agency_type = "IAS IPS Coaching"
 agency_description = dedent(
             f"""I want to create a {agency_type} agency, give me a list of agents and their 
             respective roles and tasks in a jason format, try to create roles and tasks as fine-grained as possible""")
@@ -44,24 +53,26 @@ def agencyAnalyser(type, agency_description):
         role="Researcher",
         goal="Make the best research and analysis on content about {type}",
         backstory=dedent(
-            f"""You're an expert researcher, specialized in {agency_type}. You work as a freelancer
+            f"""You're specialized in {agency_type} agency setup. You work as a freelancer
             who help customer to setup their own {agency_type} agency
-            and is now working on doing research and analysis for a new customer."""
+            and is now working on doing research and analysis for a new customer that how many 
+            agents they need and what should be role of these agents and what tasks they should perform."""
         ),
         tools=[SearchInternetTools.search_internet],
-        allow_delegation=False,
-        # output_json=Output,
-        # output_pydantic=Output,
+        allow_delegation=True,
         verbose=True,
         llm=LLMManager(
-            llm_name="mistral"
+            llm_name="gpt-3.5-turbo"
         ).connect()
     )
 
     task = Task(
         description=agency_description,
-        expected_output="Give results in json format",
+        expected_output='',
         agent=researcher,
+        # output_json=Output,
+        output_pydantic=Output,
+        output_file="output.json"
     )
 
     crew = Crew(
@@ -69,8 +80,9 @@ def agencyAnalyser(type, agency_description):
         tasks=[task],
         process=Process.hierarchical,
         verbose=2,
+        allow_delegation=True,
         manager_llm=LLMManager(
-            llm_name="llama2"
+            llm_name="gpt-4"
         ).connect()
     )
 
